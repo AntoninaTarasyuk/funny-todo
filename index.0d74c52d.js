281bddc90617cfa2e530b6c8d9e663c157966df7
AntoninaTarasyuk <96108435+AntoninaTarasyuk@users.noreply.github.com>
2022-06-17 14:28:01 +0000

    Deploying to gh-pages from @ AntoninaTarasyuk/funny-todo@d4d1fb911e8c6a8d8bfb42f5268a8f190e3ad7b5 🚀

---
+++ index.0d74c52d.js
@@ -0,0 +1,2 @@
+document.querySelector(".container").addEventListener("mouseover",(function(t){var e=t.target;e.style.top="".concat(Math.floor(Math.random()*(window.innerHeight-e.clientHeight)),"px"),e.style.left="".concat(Math.floor(Math.random()*(window.innerWidth-e.clientWidth)),"px")}));
+//# sourceMappingURL=index.0d74c52d.js.map
---
+++ index.8aca949b.js
@@ -0,0 +1,2 @@
+document.querySelector(".container").addEventListener("mouseover",(t=>{const{target:e}=t;e.style.top=`${Math.floor(Math.random()*(window.innerHeight-e.clientHeight))}px`,e.style.left=`${Math.floor(Math.random()*(window.innerWidth-e.clientWidth))}px`}));
+//# sourceMappingURL=index.8aca949b.js.map
